3305a1e4598f020ee3ff64f26728928ccf212266
tuzc0 <claudiojtz1402@gmail.com>
2026-04-27 21:50:26 -0600

    first commit

---
+++ tienda.php
@@ -0,0 +1,37 @@
+<?php 
+class tienda
+{
+    //Intentamos una conexion a un servidor de MySQL 
+    static $mysqli; 
+    static $servidor = "localhost";
+    static $usuario = "tienda_user";
+    static $password = "t13Nd@";
+    static $base_de_datos = "tienda";
+
+    public function __construct()
+    {
+        self::conectar(); 
+    }
+
+    function __destruct()
+    {
+        self::$mysqli->close(); 
+    }
+
+    protected function conectar()
+    {
+        //Esta función es la que realiza la conexión al servidor de MySQL 
+        self::$mysqli = new mysqli(self::$servidor, self::$usuario, self::$password, self::$base_de_datos);
+
+        //En caso de error lo mandamos a imprimir a la pantalla
+        //Imprimimos el número de error y su descripción
+        if (self::$mysqli->connect_error) {
+            die('Error de Conexión (' . self::$mysqli->connect_errno . ') ' . self::$mysqli->connect_error);
+        }
+    }
+
+    public function obtener_conexion()
+    {
+        return self::$mysqli;
+    }
+}
